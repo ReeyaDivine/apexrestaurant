a7643d8091c3d9a3cc148c6faed8da10fda85885
Reeya Agrawal <105407387+ReeyaDivine@users.noreply.github.com>
2023-02-13 08:54:44 +0545

    added api controller for customer

---
+++ ApexRestaurant.Repository/RepositoryModule.cs
@@ -0,0 +1,20 @@
+using Microsoft.Extensions.DependencyInjection;
+using ApexRestaurant.Repository.RCustomer;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace ApexRestaurant.Repository
+{
+    public static class RepositoryModule
+    {
+        public static void Register(IServiceCollection services)
+        {
+            services.AddDbContext<RestaurantContext>(options => options.UseSqlServer(
+                @"Server=DESKTOP-0I1I2IG\SQLEXPRESS;Initial Catalog=ApexRestaurantDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False",
+                builder => builder.MigrationsAssembly(Assembly.GetExecutingAssembly().GetName().Name)
+            ));
+
+            services.AddTransient<ICustomerRepository, CustomerRepository>();
+        }
+    }
+}
